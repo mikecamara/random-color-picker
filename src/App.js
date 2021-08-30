566c406a120f50fc6893b3330e860dd737080db2
mike83 <mike.gomes.camara@ut.ee>
2021-08-30 22:54:18 +0300

    Upload project to GitHub

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Random from './Random';
+
+function App() {
+  return (
+    <div>
+      <Random></Random>
+    </div>
+    
+  );
+}
+
+export default App;
